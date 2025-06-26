f3333f0f67fad457c4053e98c56835db7810ac69
Hakan <muhammedhakank2009@gmail.com>
2025-06-26 19:15:46 +0300

    Improvements
    * Make use of ConfigParser instead of moving all varailables to every function
    * Add tuiDoStringSelection and tuiDoWriteFolderSelection to make selection better. tuiDoWriteFolderSelection allows overwriting and tuiDoStringSelection is used on "initrd/ramdisk" question which makes app don't exit on invalid input.
    * Other code refactoring

--- OExec.py
+++ OExec.py
@@ -30,30 +30,31 @@ def tuiMainMenu():
     elif (selection == 2):
         kernel = tuiDoFileSelection("Kernel: ")
         dtb = tuiDoFileSelection("DTB: ")
-        ramdiskType = input("Ramdisk type (initrd/ramdisk): ")
-        if not (ramdiskType == "initrd" or ramdiskType == "ramdisk"):
-            print("Invalid selection.")
-            return # TODO: Make this more friendly.
+        ramdiskType = tuiDoStringSelection(("initrd", "ramdisk"),"Ramdisk type (initrd/ramdisk): ")
         ramdisk = tuiDoFileSelection("Ramdisk: ")
-        tuiBootMenu(kernel, ramdiskType, ramdisk, dtb, "")
+        
+        tuiBootMenu(createBootConfig(kernel, ramdiskType, ramdisk, dtb, ""))
     elif (selection == 3):
-        name = tuiDoUniqueFSSelection("Save name (Like a directory name): ", workdir / "RecentBoots")
+        name = tuiDoWriteFolderSelection("Save name (Like a directory name): ", workdir / "RecentBoots")
         if (not os.path.exists(workdir / "RecentBoots" / name)):
             os.mkdir(workdir / "RecentBoots" / name)
-            bootimage = tuiDoFileSelection("Boot.img file: ")
-            r = subprocess.call(("unpackbootimg", "-i", bootimage, "-o", str(workdir / "RecentBoots" / name)))
-            if r == 0:
-                imgname = os.path.basename(bootimage)
-                kernel = str(workdir / "RecentBoots" / name / (imgname + "-kernel"))
-                ramdisk = str(workdir / "RecentBoots" / name / (imgname + "-ramdisk"))
-                dtb = str(workdir / "RecentBoots" / name / (imgname + "-dt"))
-                ba = open(workdir / "RecentBoots" / name / (imgname + "-cmdline"))
-                bootargs = ba.read()
-                ba.close()
-                saveBootConfig(name, kernel, "ramdisk", ramdisk, dtb, bootargs)
-                tuiBootMenu(kernel, "ramdisk", ramdisk, dtb, bootargs)
+        bootimage = tuiDoFileSelection("Boot.img file: ")
+        r = subprocess.call(("unpackbootimg", "-i", bootimage, "-o", str(workdir / "RecentBoots" / name)))
+        if r == 0:
+            imgname = os.path.basename(bootimage)
+            kernel = str(workdir / "RecentBoots" / name / (imgname + "-kernel"))
+            ramdisk = str(workdir / "RecentBoots" / name / (imgname + "-ramdisk"))
+            dtb = str(workdir / "RecentBoots" / name / (imgname + "-dt"))
+            ba = open(workdir / "RecentBoots" / name / (imgname + "-cmdline"))
+            bootargs = ba.read()
+            ba.close()
+            cfg = createBootConfig(kernel, "ramdisk", ramdisk, dtb, bootargs)
+            saveBootConfig(name, cfg)
+            tuiBootMenu(cfg)
     elif (selection > 3 and len(boots) >= selection - 3):
-        loadBootConfig(boots[selection - 4], tuiBootMenu)
+        cfg = loadBootConfig(boots[selection - 4])
+        if (cfg != None):
+            tuiBootMenu(cfg)
     else:
         print("Invalid Selection.")
     tuiMainMenu()
@@ -77,6 +78,13 @@ def tuiDoSelection(): # Make numeric selection.
     else:
         return tuiDoSelection()
 
+def tuiDoStringSelection(l, text):
+    sel = input(text)
+    if sel in l:
+        return sel
+    else:
+        return tuiDoStringSelection(l, text)
+
 def tuiDoFileSelection(prompt):
     sel = input(prompt)
     if os.path.exists(sel) and os.path.isfile(sel):
@@ -86,39 +94,50 @@ def tuiDoFileSelection(prompt):
     
 def tuiDoUniqueFSSelection(prompt, append):
     sel = input(prompt)
-    if (sel == ""):
-        return sel
     if not os.path.exists(append / sel):
         return sel
     else:
         return tuiDoUniqueFSSelection(prompt, append)
 
-
-def tuiBootMenu(kernel, ramdiskType, ramdisk, dtb, bootargsAppend): # Menu after boot is ready.
-    action = input("What would you like to do? ([a]rgs, [i]nfo, [s]ave, [b]oot, [e]xit): ")
-    if (action[0] == "a"):
-        bootargsAppendNew = input("boot arguments [" + bootargsAppend + "]: ")
-        if (bootargsAppendNew != ""):
-            bootargsAppend = bootargsAppendNew
-        tuiBootMenu(kernel, ramdiskType, ramdisk, dtb, bootargsAppend)
-    elif (action[0] == "i"):
-        print("Kernel: " + kernel)
-        print("Ramdisk type: " + ramdiskType)
-        print("Ramdisk: " + ramdisk)
-        print("DTB: " + dtb)
-        print("Boot arguments append: " + bootargsAppend)
-        tuiBootMenu(kernel, ramdiskType, ramdisk, dtb, bootargsAppend)
-    elif (action[0] == "s"):
-        saveConfig = tuiDoUniqueFSSelection("Type the name for the save (like a directory name): ", workdir / "RecentBoots")
-        if (saveConfig != ""):
-            saveBootConfig(saveConfig, kernel, ramdiskType, ramdisk, dtb, bootargsAppend)
-        tuiBootMenu(kernel, ramdiskType, ramdisk, dtb, bootargsAppend)
-    elif (action[0] == "b"):
-        boot(kernel, ramdiskType, ramdisk, dtb, bootargsAppend)
-    elif (action[0] == "e"):
-        print("Exiting to main menu...")
+def tuiDoWriteFolderSelection(prompt, append):
+    sel = input(prompt)
+    if not os.path.exists(append / sel):
+        return sel
+    elif os.path.isdir(append / sel) and input("Do you want to overwrite (y/[n])? ") == "y":
+        return sel
     else:
-        tuiBootMenu(kernel, ramdiskType, ramdisk, dtb, bootargsAppend)
+        return tuiDoWriteFolderSelection(prompt, append)
+
+def tuiBootMenu(config): # Menu after boot is ready.
+    kernel = config["bootconfig"]["kernel"]
+    ramdiskType = config["bootconfig"]["ramdiskType"]
+    ramdisk = config["bootconfig"]["ramdisk"]
+    dtb = config["bootconfig"]["dtb"]
+    bootargsAppend = config["bootconfig"]["bootargsAppend"]
+    action = input("What would you like to do? ([a]rgs, [i]nfo, [s]ave, [b]oot, [e]xit): ")
+    loop = True
+    if len(action) > 0:
+        if (action[0] == "a"):
+            bootargsAppendNew = input("boot arguments [" + bootargsAppend + "]: ")
+            if (bootargsAppendNew != ""):
+                config["bootconfig"]["bootargsAppend"] = bootargsAppendNew
+        elif (action[0] == "i"):
+            print("Kernel: " + kernel)
+            print("Ramdisk type: " + ramdiskType)
+            print("Ramdisk: " + ramdisk)
+            print("DTB: " + dtb)
+            print("Boot arguments append: " + bootargsAppend)
+        elif (action[0] == "s"):
+            saveConfig = tuiDoWriteFolderSelection("Type the name for the save (like a directory name): ", workdir / "RecentBoots")
+            if (saveConfig != ""):
+                saveBootConfig(saveConfig, config)
+        elif (action[0] == "b"):
+            boot(config)
+        elif (action[0] == "e"):
+            print("Exiting to main menu...")
+            loop = False
+    if loop:
+        tuiBootMenu(config)
 
 
 
@@ -143,9 +162,7 @@ def getRecentBoots():
     boots = [f for f in os.listdir(bootspath) if os.path.isdir(os.path.join(bootspath, f))]
     return boots
 
-def saveBootConfig(configname, kernel, ramdiskType, ramdisk, dtb, bootargsAppend):
-    if (not os.path.exists(workdir / "RecentBoots" / configname)):
-        os.mkdir(workdir / "RecentBoots" / configname)
+def createBootConfig(kernel, ramdiskType, ramdisk, dtb, bootargsAppend): # Create boot config from parameters.
     config = configparser.ConfigParser()
     config["bootconfig"] = {}
     config["bootconfig"]["kernel"] = kernel
@@ -153,23 +170,29 @@ def saveBootConfig(configname, kernel, ramdiskType, ramdisk, dtb, bootargsAppend
     config["bootconfig"]["ramdisk"] = ramdisk
     config["bootconfig"]["dtb"] = dtb
     config["bootconfig"]["bootargsAppend"] = bootargsAppend
+    return config
+    
+def saveBootConfig(configname, config):
+    if (not os.path.exists(workdir / "RecentBoots" / configname)):
+        os.mkdir(workdir / "RecentBoots" / configname)
     savefile = open(workdir / "RecentBoots" / configname / "config.ini", 'w')
     config.write(savefile)
 
-def loadBootConfig(configname, func):
+def loadBootConfig(configname):
     if (os.path.exists(workdir / "RecentBoots" / configname)):
         config = configparser.ConfigParser()
         config.read(workdir / "RecentBoots" / configname / "config.ini")
         # Execute the function that was given as parameter.
-        func(
-            config["bootconfig"]["kernel"],
-            config["bootconfig"]["ramdiskType"],
-            config["bootconfig"]["ramdisk"],
-            config["bootconfig"]["dtb"],
-            config["bootconfig"]["bootargsAppend"])
-    #Else, just ignore it.
+        return config
+    return None
 
-def boot(kernel, ramdiskType, ramdisk, dtb, bootargsAppend):
+def boot(config):
+    kernel = config["bootconfig"]["kernel"]
+    ramdiskType = config["bootconfig"]["ramdiskType"]
+    ramdisk = config["bootconfig"]["ramdisk"]
+    dtb = config["bootconfig"]["dtb"]
+    bootargsAppend = config["bootconfig"]["bootargsAppend"]
+
     global deviceName
     config = configparser.ConfigParser()
     config.read("./DeviceSpecific/" + deviceName + ".ini") # getDevice already checks if this exists.
